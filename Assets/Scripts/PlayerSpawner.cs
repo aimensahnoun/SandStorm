fa58fade138c2f623433e0b1984bbf986be03230
Aimen Sahnoun <aimensahnoun@outlook.com>
2022-06-28 11:59:32 +0300

    Player color 2

--- Assets/Scripts/PlayerSpawner.cs
+++ Assets/Scripts/PlayerSpawner.cs
@@ -21,8 +21,8 @@ public class PlayerSpawner : MonoBehaviour
         Vector3 randomSpawnPosition = new Vector3(Random.Range(spawnCoordinateMinX, spawnCoordinateMaxX), 8f,
                 Random.Range(spawnCoordinateMinZ, spawnCoordinateMaxZ));
         player = PhotonNetwork.Instantiate("Player", randomSpawnPosition, Quaternion.identity);
-        player.GetPhotonView().RPC("changeColor", RpcTarget.AllBuffered);
-        
+        player.GetComponent<Renderer>().material.color = Color.green;
+
 
     }
 
